3b632a899afe6321cf385a259b6b88a344ce587b
shwetathakkar <shweta.thakkar@cloudcms.com>
2018-02-14 18:31:24 -0500

    commented

--- cricket-theme/index.js
+++ cricket-theme/index.js
@@ -2,12 +2,12 @@ define(function(require, exports, module) {
 
     var UI = require("ui");
     var moduleId = module.uri.match(/^.+(_modules[^\/]+)\/.*/)[1];
-
+    console.log("moduleID in the porgram" + moduleId);
     // register the theme: "cricket"
     UI.registerTheme({
         "key": "cricket",
         "title": "Cricket",
-        "module": "cricket-theme.js"
+        "module": moduleId + "/cricket-theme.js"
     });
 
 });
