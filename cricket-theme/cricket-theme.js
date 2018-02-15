fc90c887ce0d2f4b2670de23595c90f6d094d1dc
shwetathakkar <shweta.thakkar@cloudcms.com>
2018-02-15 14:52:49 -0500

    modified

--- cricket-theme/cricket-theme.js
+++ cricket-theme/cricket-theme.js
@@ -1,10 +1,11 @@
 define(function(require, exports, module) {
 
+    require("css!./cricket-theme.css");
+
     require("css!bootstrap/../../css/bootstrap.css");
     require("css!app/../../main.css");
 
     require("css!themes/theme-reset.css");
     require("css!./theme.css");
-
-    require("css!./cricket-theme.css");
+    
 });
